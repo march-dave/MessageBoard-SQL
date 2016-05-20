7bce34d54099ac24c4b1f3acfb8106cdb10a6cea
march-dave <ysangy@gmail.com>
2016-05-19 17:05:49 -0700

    MVP?

--- models/car.js
+++ models/car.js
@@ -9,32 +9,6 @@ db.run(`CREATE TABLE IF NOT EXISTS cars(
   make text,
   model text,
   year integer)`);
-
-// exports.create = function(car, cb) {
-//   // db.serialize(function() {
-//   //   var stmt = db.prepare("INSERT INTO cars VALUES (?, ?, ?, ?)");
-//   //   stmt.run(uuid(), car.make, car.model, car.year);
-//   //   stmt.finalize(cb);
-//   // })
-//
-//   db.run('INSERT INTO cars (id, make, model, year) VALUES (?, ?, ?, ?)',
-//     uuid(),
-//     car.make,
-//     car.model,
-//     car.year,
-//     (err) => {
-//       if(err) return cb(err);
-//       // db.get('SELECT * FROM cars WHERE numid = (SELECT MAX (numid) FROM cars);', cb)
-//       db.get('SELECT * FROM cars WHERE numid = (SELECT MAX (numid) FROM cars)', function(err, cars) {
-//         cb(err, 'cars 111111');
-//       });
-//
-//       // db.all('SELECT * FROM cars', function(err, cars) {
-//       //   cb(err, cars);
-//       // });
-//     });
-// };
-
 exports.create = function(car, cb) {
 
   db.run('INSERT INTO cars (id, make, model, year) VALUES (?, ?, ?, ?)',
@@ -44,9 +18,7 @@ exports.create = function(car, cb) {
     car.year,
 
     (err) => {
-
       if(err) return cb(err);
-
       db.get('SELECT * FROM cars WHERE numid = (SELECT MAX (numid) FROM cars);', cb)
       }
     );
@@ -69,27 +41,12 @@ exports.update = function(id, car, cb) {
 };
 
 exports.removeById = function(id, cb) {
-  // db.all(`DELETE FROM  Cars  WHERE ID = '${id}'`, function(err, cars) {
-  //   cb(err, cars);
-  // });
-  db.all(`DELETE FROM  Cars  WHERE ID = '${id}'`, (err, cars) => {
+    db.all(`DELETE FROM  Cars  WHERE ID = '${id}'`, (err, cars) => {
     if (err) return cb (err);
 
-    db.get('SELECT * FROM cars', cb(err, cars) );
-    // db.get('SELECT * FROM cars ORDER BY 1 DESC', function(err, cars) {
-    //   // cb(err, cars);
-    //   cb(err, cars);
-    // } );
-    // cb(err, cars);
+    cb(err, cars);
   });
 
-    // (err) => {
-    //
-    //    if(err) return cb(err);
-    //
-    //    db.get('SELECT * FROM todos WHERE ID = (SELECT MAX (ID) FROM todos);', cb)
-    //
-    //  });
 
 
 };
--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@
 $(document).ready(init);
 
 function init() {
-  // $('table').on('click', openModel);
+  $('table').on('click', openModel);
   // $('form.submitform').submit(submitCar);
   $('form.submitform').submit(submitNewCar);
   $('.carList').on('click', '.isDelete', deleteCar);
@@ -19,8 +19,6 @@ function deleteCar(e) {
   })
   .done(function(data) {
     $(`td:contains(${id})`).parent().remove();
-
-    console.log('data', data);
 
   })
   .fail(function (err) {
@@ -56,13 +54,8 @@ function submitNewCar(e) {
      $car.find('.model').text(data.model);
      $car.find('.year').text(data.year);
 
-    //  $car.data('id', data);
      $('.carList').append($car);
-
-    console.log('data', data);
-
      $('.modal').modal('hide');
-
   })
   .fail(function (err) {
     console.log(err);
@@ -85,8 +78,6 @@ function submitCar(e) {
     year: year
   }
 
-  // console.log('car', car);
-
   var url = `api/cars/${id}`;
   $.ajax({
     url: url,
@@ -94,22 +85,6 @@ function submitCar(e) {
     data: car
   })
   .done(function(data) {
-  // var $list = dataList(data);
-
-
-
-  // var id = $(this).parent().parent().children()[0].textContent;
-  // var url = `api/cars/${id}`;
-  //
-  // $(`td:contains(${id})`).parent().remove();
-  // $(`td:contains(${id})`).parent().remove();
-  // $(`td:contains(${id})`).parent().remove();
-  // $(`td:contains(${id})`).parent().remove();
-
-
-
-
-
 
    $('.modal').modal('hide');
 
@@ -120,6 +95,11 @@ function submitCar(e) {
 }
 
 function openModel(e) {
+  var s = $(e.target).is('input') ;
+  if (s === true) {
+    return;
+  }
+
   $('#myModal').modal('show');
   var id = $(e.target).closest('tr').children(0)[0].textContent;
   var make = $(e.target).closest('tr').children(0)[1].textContent;
@@ -132,129 +112,60 @@ function openModel(e) {
   $('#year').val(year);
 }
 
-function removelist(event) {
-  var index = $(this).closest('tr').index();
-  var url = 'api/cars/'+ id;
-  $.ajax({
-  })
-  .done(function(data) {
-      var $list = dataList(data);
-  })
-  .fail(function (err) {
-    console.log(err);
-  });
-  renderList();
-}
-
-function getItem() {
-
-    // localhost:3000/api/cars/a509e55f-a352-4d26-8ce2-d4b5f6db7b81
-    // var index = $(this).closest('tr').index();
-    var id = $(this).parent().parent().children()[0].textContent;
-
-    var url = 'api/cars/'+ id;
-    $.ajax({
-      url: url,
-      type: 'GET'
-    })
-    .done(function(data) {
-        var $list = dataList(data);
-        console.log('$list', $list);
-        console.log('data', data);
-    })
-    .fail(function (err) {
-      console.log(err);
-    });
-    renderList();
-}
-
-function updateList(event) {
-
-  var index = $(this).closest('tr').index();
-  var id = $(this).parent().parent().children()[0].textContent;
-
-  console.log('ld', id);
-
-  var url = 'api/cars/'+ id;
-  $.ajax({
-    url: url,
-    type: 'PUT'
-  })
-  .done(function(data) {
-      var $list = dataList(data);
-  })
-  .fail(function (err) {
-    console.log(err);
-  });
-  renderList();
-}
-
-function renderList() {
-  // $.post('/api/bananas/:id', {varitety: 'cavendish'})
-  // .done()
-  var url = 'api/cars';
-  $.ajax({
-    url: url,
-  })
-  .done(function(data) {
-      var $list = dataList(data);
-  })
-  .fail(function (err) {
-    console.log(err);
-  });
-}
-
-function eventSplit(event) {
-
-	if (typeof event != 'undefined') {
-
-		if (  $('button').is(event.target) ){
-			removeContact(event);
-		} else if ( $('img.img-circle').is(event.target) ) {
-			updateContact(event, $(this).index() );
-		}
-	}
-}
-
-var contactArray = [];
-function saveContact() {
-
-	var image = $('#image').val();
-
-	var name = $('#name').val();
-	var phone = $('#phone').val();
-	var address = $('#address').val();
-	var email = $('#email').val();
-	var birthday = $('#birthday').val();
-	var favorite = $('#favorite').val();
-
-	if (contactArray.length > 0) {
-		contactArray = [];
-	}
-
-	contactArray.push(image);
-	contactArray.push(name);
-	contactArray.push(phone);
-	contactArray.push(address);
-	contactArray.push(email);
-	contactArray.push(birthday);
-	contactArray.push(favorite);
-
-	$('#image').val('');
-	$('#name').val('');
-	$('#phone').val('');
-	$('#address').val('');
-	$('#email').val('');
-	$('#birthday').val('');
-	$('#favorite').val('');
-
-  $('#btnSave').show();
-  $('#btnUpdate').hide();
-
-	addContact();
-
-}
-
+// function removelist(event) {
+//   var index = $(this).closest('tr').index();
+//   var url = 'api/cars/'+ id;
+//   $.ajax({
+//   })
+//   .done(function(data) {
+//       var $list = dataList(data);
+//   })
+//   .fail(function (err) {
+//     console.log(err);
+//   });
+//   renderList();
+// }
+//
+// function getItem() {
+//     var id = $(this).parent().parent().children()[0].textContent;
+//
+//     var url = 'api/cars/'+ id;
+//     $.ajax({
+//       url: url,
+//       type: 'GET'
+//     })
+//     .done(function(data) {
+//         var $list = dataList(data);
+//         console.log('$list', $list);
+//         console.log('data', data);
+//     })
+//     .fail(function (err) {
+//       console.log(err);
+//     });
+//     renderList();
+// }
+//
+// function updateList(event) {
+//
+//   var index = $(this).closest('tr').index();
+//   var id = $(this).parent().parent().children()[0].textContent;
+//
+//   console.log('ld', id);
+//
+//   var url = 'api/cars/'+ id;
+//   $.ajax({
+//     url: url,
+//     type: 'PUT'
+//   })
+//   .done(function(data) {
+//       var $list = dataList(data);
+//   })
+//   .fail(function (err) {
+//     console.log(err);
+//   });
+//   renderList();
+// }
+//
 // function renderList() {
 //   // $.post('/api/bananas/:id', {varitety: 'cavendish'})
 //   // .done()
@@ -269,35 +180,101 @@ function saveContact() {
 //     console.log(err);
 //   });
 // }
-
-function dataList(data) {
-  $('table').empty();
-  var count = 0;
-
-  for (var i=0; i<data.length; i++) {
-    var array = [];
-    var $list = $('<tr>').addClass('dataList');
-    // will be hiddn field
-    var $id = $('<td>').text(data[i].id);
-    var $make = $('<td>').text(data[i].make);
-    var $model = $('<td>').text(data[i].model);
-    var $year = $('<td>').text(data[i].year);
-    var $buttonU = $('<td>').append('<button>Update').addClass('btnUpdate');
-    var $buttonD = $('<td>').append('<button>Delete').addClass('btnDelete');
-
-    array.push($list);
-    array.push($id);
-    array.push($make);
-    array.push($model);
-    array.push($year);
-    array.push($buttonU);
-    array.push($buttonD);
-
-    $list.append(array);
-    $('table').append($list);
-    array = '';
-    $list = '';
-  }
-
-  return $list;
-}
+//
+// function eventSplit(event) {
+//
+// 	if (typeof event != 'undefined') {
+//
+// 		if (  $('button').is(event.target) ){
+// 			removeContact(event);
+// 		} else if ( $('img.img-circle').is(event.target) ) {
+// 			updateContact(event, $(this).index() );
+// 		}
+// 	}
+// }
+//
+// var contactArray = [];
+// function saveContact() {
+//
+// 	var image = $('#image').val();
+//
+// 	var name = $('#name').val();
+// 	var phone = $('#phone').val();
+// 	var address = $('#address').val();
+// 	var email = $('#email').val();
+// 	var birthday = $('#birthday').val();
+// 	var favorite = $('#favorite').val();
+//
+// 	if (contactArray.length > 0) {
+// 		contactArray = [];
+// 	}
+//
+// 	contactArray.push(image);
+// 	contactArray.push(name);
+// 	contactArray.push(phone);
+// 	contactArray.push(address);
+// 	contactArray.push(email);
+// 	contactArray.push(birthday);
+// 	contactArray.push(favorite);
+//
+// 	$('#image').val('');
+// 	$('#name').val('');
+// 	$('#phone').val('');
+// 	$('#address').val('');
+// 	$('#email').val('');
+// 	$('#birthday').val('');
+// 	$('#favorite').val('');
+//
+//   $('#btnSave').show();
+//   $('#btnUpdate').hide();
+//
+// 	addContact();
+//
+// }
+//
+// // function renderList() {
+// //   // $.post('/api/bananas/:id', {varitety: 'cavendish'})
+// //   // .done()
+// //   var url = 'api/cars';
+// //   $.ajax({
+// //     url: url,
+// //   })
+// //   .done(function(data) {
+// //       var $list = dataList(data);
+// //   })
+// //   .fail(function (err) {
+// //     console.log(err);
+// //   });
+// // }
+//
+// function dataList(data) {
+//   $('table').empty();
+//   var count = 0;
+//
+//   for (var i=0; i<data.length; i++) {
+//     var array = [];
+//     var $list = $('<tr>').addClass('dataList');
+//     // will be hiddn field
+//     var $id = $('<td>').text(data[i].id);
+//     var $make = $('<td>').text(data[i].make);
+//     var $model = $('<td>').text(data[i].model);
+//     var $year = $('<td>').text(data[i].year);
+//     var $buttonU = $('<td>').append('<button>Update').addClass('btnUpdate');
+//     var $buttonD = $('<td>').append('<button>Delete').addClass('btnDelete');
+//
+//     array.push($list);
+//     array.push($id);
+//     array.push($make);
+//     array.push($model);
+//     array.push($year);
+//     array.push($buttonU);
+//     array.push($buttonD);
+//
+//     $list.append(array);
+//     $('table').append($list);
+//     array = '';
+//     $list = '';
+//   }
+//
+//   return $list;
+// }
